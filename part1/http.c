13500e908a4afd0f05934d4f92891d927f8b56e7
Kevin Tu <kevin.xander.tu@gmail.com>
2025-04-29 21:25:00 -0500

    Write write_http_response

--- part1/http.c
+++ part1/http.c
@@ -28,9 +28,24 @@ const char *get_mime_type(const char *file_extension) {
     return NULL;
 }
 
+static const char* not_found_string =
+"HTTP/1.0 404 Not Found\r\n"
+"Content-Length: 0\r\n"
+"\r\n";
+
+static const char* ok_string =
+"HTTP/1.0 200 OK\r\n"
+"Content-Type: %s\r\n"
+"Content-Length: %d\r\n"
+"\r\n";
+
 int get_line(int fd, char* line_buf, int n) {
     for (int i = 0; i < n; i++) {
-        if (read(fd, line_buf + i, 1) != 1) { break; }
+        if (read(fd, line_buf + i, 1) < 0) {
+            perror("read");
+            return -1;
+        }
+        else if (!*(line_buf + i)) { break; }
 
         if (*(line_buf + i) == '\n' && i) {
             if (*(line_buf + i - 1) == '\r') {
@@ -47,7 +62,9 @@ int read_http_request(int fd, char *resource_name) {
 
     char line_buf[BUFSIZE] = { 0 };
     // Read a line from fd
-    get_line(fd, line_buf, BUFSIZE);
+    if (get_line(fd, line_buf, BUFSIZE) < 0) {
+        return -1;
+    }
     while (*line_buf) {
         // Break if it's the empty line
         if (strcmp(line_buf, "\r\n") == 0) { break; }
@@ -62,13 +79,63 @@ int read_http_request(int fd, char *resource_name) {
         // Reset the line buffer
         memset(line_buf, 0, BUFSIZE);
         // Read the next line
-        get_line(fd, line_buf, BUFSIZE);
+        if (get_line(fd, line_buf, BUFSIZE) < 0) {
+            return -1;
+        }
     }
 
     return ret_val;
 }
 
 int write_http_response(int fd, const char *resource_path) {
-    // TODO Not yet implemented
+    struct stat statinfo;
+    if (stat(resource_path, &statinfo) < 0) {
+        printf("%s\n", resource_path);
+        printf("404\n");
+        if (write(fd, not_found_string, strlen(not_found_string) + 1) < 0) {
+            perror("write");
+            return -1;
+        }
+        return 0;
+    }
+ 
+    // Open content file
+    int resource_fd = open(resource_path, O_RDONLY);
+    if (resource_fd < 0) {
+        perror("open");
+        return -1;
+    }
+
+    char buf[BUFSIZE] = { 0 };
+
+    // Write the top of the HTTP response
+    const char* mime_type = get_mime_type(strrchr(resource_path, '.'));
+    snprintf(buf, BUFSIZE, ok_string, mime_type, statinfo.st_size);
+    write(fd, buf, strlen(buf));
+
+    memset(buf, 0, BUFSIZE);
+
+    // Write the body of the HTTP response
+    while (1) {
+        int bytes_read = read(resource_fd, buf, BUFSIZE);
+        if (bytes_read < 0) {
+            perror("read");
+            close(resource_fd);
+            return -1;
+        }
+        else if (bytes_read == 0) {
+            break;
+        }
+        else if (write(fd, buf, bytes_read) < 0) {
+            perror("write");
+            close(resource_fd);
+            return -1;
+        }
+    }
+
+    if (close(resource_fd) < 0) {
+        perror("close");
+        return -1;
+    }
     return 0;
 }
